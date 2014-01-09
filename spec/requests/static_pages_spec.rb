d461b0465d4ac311fa438e99d3073b19dce45b8e
lutz <lutz.biedinger@gmail.com>
2014-01-09 13:05:07 +0000

    finish feature

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "#{base_title}" }
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
