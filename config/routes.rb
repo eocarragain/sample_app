af6c30023cc4aca9529ff8b0c4f898c482ca184a
Eoghan Ó Carragáin <eoghan.ocarragain@gmail.com>
2014-01-09 10:35:27 +0000

    added about page and tests for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
-  #LUTZ COMMENT & Eoghan
-  #DOUBLE LUTZ COMMENT
   get "static_pages/help"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
