18e47b4dcf1321646c36dae0d78a0a5ebcccd5ea
lutz <lutz.biedinger@gmail.com>
2014-01-17 10:42:09 +0000

    go through exercises until listing 10.24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -54,8 +55,8 @@ class UsersController < ApplicationController
 
     # Before filters
 
-    def signed_in_user
-      unless signed_in?
+     def signed_in_user
+        unless signed_in?
         store_location
         redirect_to signin_url, notice: "Please sign in."
       end
